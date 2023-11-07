b0aff639be7a93258910466ab9cb0cea0f99cf81
SJ.Lee <eagleduk@gmail.com>
2023-11-07 23:20:59 +0900

    section 03 finish

--- 03. react starting/src/components/Expenses/ExpenseItem.js
+++ 03. react starting/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,15 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem({ expense: { date, title, amount } }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
