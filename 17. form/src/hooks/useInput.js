927db1d925a85dfe7327941944719f1f2a77032b
SJ.Lee <eagleduk@gmail.com>
2023-11-30 23:43:42 +0900

    17. form > basic form use Reducer in custom hook

--- 17. form/src/hooks/useInput.js
+++ 17. form/src/hooks/useInput.js
@@ -1,27 +1,43 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const INITAILVALUE = {
+  value: "",
+  isTouched: false,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === "RESET") {
+    return INITAILVALUE;
+  }
+  return INITAILVALUE;
+};
 
 function useInput(validate) {
-  const [value, setValue] = useState("");
-  const [inputTouched, setInputTouched] = useState(false);
+  const [inputState, inputDispatch] = useReducer(reducer, INITAILVALUE);
 
-  const isValid = validate(value);
-  const isInvalid = inputTouched && !isValid;
+  const isValid = validate(inputState.value);
+  const isInvalid = inputState.isTouched && !isValid;
 
   const inputChangeHandler = (event) => {
-    setValue(event.target.value);
+    inputDispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setInputTouched(true);
+    inputDispatch({ type: "BLUR" });
   };
 
   const inputReset = () => {
-    setValue("");
-    setInputTouched(false);
+    inputDispatch({ type: "RESET" });
   };
 
   return {
-    value,
+    value: inputState.value,
     isValid,
     isInvalid,
     inputChangeHandler,
