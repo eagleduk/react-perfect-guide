3eef1bf11488265578e1895a747d24c3c149f177
SJ.Lee <eagleduk@gmail.com>
2023-12-04 22:15:39 +0900

    19. redux > use redux hook

--- 19. redux/README.md
+++ 19. redux/README.md
@@ -3,3 +3,7 @@
 - redux 는 상태 관리 라이브러리이다.
 - react 전용이 아니라 일반 JS, 또는 다른 언어에서도 사용이 가능하다.
 - react *context* 와 비슷하게 구현한다.
+
+1. reducer 함수 구현(`./src/store/index.js`)
+2. Provider Component 로 상태를 사용하고자 하는 Component 를 감싼다(`./src/index.js`)
+3. useSelecter, useStore 또는 useDispatch Custom Hook을 사용하여 현재 상태 및 상태 변경 reducer 를 호출한다(`./src/components/Counter.js`)
--- 19. redux/src/components/Counter.js
+++ 19. redux/src/components/Counter.js
@@ -1,22 +1,28 @@
-import { useState } from "react";
 import classes from "./Counter.module.css";
 
-import store from "../store/index";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const [isIncrement, setIsIncrement] = useState(true);
-  const { counter } = store.getState();
-  const toggleCounterHandler = () => {
-    if (isIncrement) store.dispatch({ type: "increment" });
-    else store.dispatch({ type: "decrement" });
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
-    setIsIncrement((prev) => !prev);
+  const handleIncrementButtonEvent = () => {
+    dispatch({ type: "increment" });
   };
+  const handleDecrementButtonEvent = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrementButtonEvent}>Increment</button>
+        <button onClick={handleDecrementButtonEvent}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
