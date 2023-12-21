284bd7b635c0f200eb1d26a6ec5e4cc6bb05a62d
SJ.Lee <eagleduk@gmail.com>
2023-12-21 22:43:19 +0900

    24. tanstack > list/search query, create mutation, invalidateQueries

---
+++ 24. tanstack/src/components/Events/NewEvent.jsx
@@ -0,0 +1,41 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from "@tanstack/react-query";
+
+import Modal from '../UI/Modal.jsx';
+import EventForm from './EventForm.jsx';
+import { createNewEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+
+export default function NewEvent() {
+  const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["events"]})
+      navigate("/events");
+    }
+  })
+
+  function handleSubmit(formData) {
+    mutate({event: formData})
+  }
+
+  return (
+    <Modal onClose={() => navigate('../')}>
+      <EventForm onSubmit={handleSubmit}>
+          {isPending && "Submit...."}
+          {!isPending &&
+            <>
+            {isError && <ErrorBlock title={"create Error"} message={error?.info.message || "create error"} />}
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+            </>
+          }
+      </EventForm>
+    </Modal>
+  );
+}
