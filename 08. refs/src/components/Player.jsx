84527f949bab58e7047cb0b3b9b9e5780c6645a1
SJ.Lee <eagleduk@gmail.com>
2024-01-03 21:48:09 +0900

    08. refs > use react reference

--- 08. refs/src/components/Player.jsx
+++ 08. refs/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const inputRef = useRef();
+
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleButtonClickEvent() {
+    setPlayerName(inputRef.current.value);
+    inputRef.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputRef} />
+        <button onClick={handleButtonClickEvent}>Set Name</button>
       </p>
     </section>
   );
