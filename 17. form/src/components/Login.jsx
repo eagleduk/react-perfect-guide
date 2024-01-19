61536fdbbe4aa9fec57d9c109b14749e2d037288
SJ.Lee <eagleduk@gmail.com>
2024-01-19 14:38:39 +0900

    17. form > individual status submit

--- 17. form/src/components/Login.jsx
+++ 17. form/src/components/Login.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitEvent = (event) => {
+    event.preventDefault();
+    console.log("email: " + email + ", password: " + password);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmitEvent}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
       </div>
 
