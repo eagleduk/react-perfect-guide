aa379438ef1f32a28a1fe4384ce8493cc43e1436
SJ.Lee <eagleduk@gmail.com>
2024-01-30 23:09:47 +0900

    27. react app test > http fetch test use jest mock

--- 27. react app test/src/components/Async.test.js
+++ 27. react app test/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async Test", () => {
   test("Http fetch Test", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "i1", title: "title" }],
+    });
     render(<Async />);
 
     const listItems = await screen.findAllByRole("listitem");
