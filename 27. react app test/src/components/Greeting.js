67422d3feb7da2c6cd8864292a88cae4d451085b
SJ.Lee <eagleduk@gmail.com>
2024-01-30 22:47:28 +0900

    27. react app test > connection children component test

--- 27. react app test/src/components/Greeting.js
+++ 27. react app test/src/components/Greeting.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import Output from "./Output";
 
 export default function Greeting() {
   const [change, setChange] = useState(false);
   return (
     <div>
       <h1>Hello World</h1>
-      {!change && <p>Nice to meet you</p>}
-      {change && <p>Good Bye</p>}
+      {!change && <Output>Nice to meet you</Output>}
+      {change && <Output>Good Bye</Output>}
 
       <button onClick={() => setChange(true)}>Change</button>
     </div>
