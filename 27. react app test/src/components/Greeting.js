c9b388ab9a00273e7f19e756343c79671ecf2364
SJ.Lee <eagleduk@gmail.com>
2024-01-30 22:35:36 +0900

    27. react app test > user Events

--- 27. react app test/src/components/Greeting.js
+++ 27. react app test/src/components/Greeting.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 export default function Greeting() {
+  const [change, setChange] = useState(false);
   return (
     <div>
       <h1>Hello World</h1>
-      <p>Nice to meet you!</p>
+      {!change && <p>Nice to meet you</p>}
+      {change && <p>Good Bye</p>}
+
+      <button onClick={() => setChange(true)}>Change</button>
     </div>
   );
 }
--- 27. react app test/src/components/Greeting.test.js
+++ 27. react app test/src/components/Greeting.test.js
@@ -1,15 +1,37 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting Test Suite", () => {
   test("Hello World Text test", () => {
     // Arrange
     render(<Greeting />);
-
     // Act
 
     // Assert
     const target = screen.getByText("Hello World");
+    expect(target).toBeInTheDocument();
+  });
+
+  test("Before Change Text", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // not perfectly corret option add
+    const target = screen.getByText("Nice to meet", { exact: false });
+    expect(target).toBeInTheDocument();
+  });
+
+  test("After Change Text", () => {
+    // Arrange
+    render(<Greeting />);
+    const button = screen.getByRole("button");
+
+    // Act
+    userEvent.click(button);
+
+    // Assert
+    const target = screen.getByText("Good Bye");
     expect(target).toBeInTheDocument();
   });
 });
