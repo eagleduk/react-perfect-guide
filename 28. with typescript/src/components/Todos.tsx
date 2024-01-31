0a6a03920418a52a2d3bee96dfe11bf3b33be3e2
SJ.Lee <eagleduk@gmail.com>
2024-01-31 23:30:18 +0900

    28. with typescript > send state and remove using prototype bind

--- 28. with typescript/src/components/Todos.tsx
+++ 28. with typescript/src/components/Todos.tsx
@@ -15,7 +15,7 @@ type a = React.FC<TT>
 const Todos: a = (props) => {
     return <ul className={classes.todos}>
         {
-            props.items.map(item => <TodoItem key={item.id} item={item} onRemoveTodo={props.onRemoveTodo} />)
+            props.items.map(item => <TodoItem key={item.id} item={item} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />)
         }
     </ul>
 }
